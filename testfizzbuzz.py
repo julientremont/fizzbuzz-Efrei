06b8481a708fb163c7bb21aabeb36ae97fe5e0a9
julientremont <tremont.julien@gmail.com>
2025-02-06 12:06:50 +0100

    comentaire dans les codes

--- testfizzbuzz.py
+++ testfizzbuzz.py
@@ -1,12 +1,13 @@
-import unittest
-import fizzbuzz1 as fb1
+import unittest  # Import du module unittest pour effectuer des tests unitaires
+import fizzbuzz1 as fb1  # Import du module fizzbuzz1 contenant la fonction fizzbuzStage1
 
-class TestFizzbuzz(unittest.TestCase):
-    def test_fizzbuzz_5(self):
+class TestFizzbuzz(unittest.TestCase):  # Définition d'une classe de test qui hérite de unittest.TestCase
+    
+    def test_fizzbuzz_5(self):  # Teste si l'entrée 5 retourne "Buzz"
         self.assertEqual(fb1.fizzbuzStage1(5), "Buzz")
 
-    def test_fizzbuzz_3(self):
+    def test_fizzbuzz_3(self):  # Teste si l'entrée 3 retourne "Fizz"
         self.assertEqual(fb1.fizzbuzStage1(3), "Fizz")
     
-    def test_fizzbuzz_15(self):
+    def test_fizzbuzz_15(self):  # Teste si l'entrée 15 retourne "FizzBuzz"
         self.assertEqual(fb1.fizzbuzStage1(15), "FizzBuzz")
